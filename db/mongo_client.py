bdb865f48f6218c0345adc7b884d6410e3ecd502
crossboww <krishkonda89@gmail.com>
2025-08-02 11:27:06 +0530

    updated mongo_Uri for streamlit

--- db/mongo_client.py
+++ db/mongo_client.py
@@ -1,10 +1,11 @@
 from pymongo import MongoClient
 from dotenv import load_dotenv
+import streamlit as st
 import os
 
 load_dotenv()
 
 def get_mongo_client():
-    mongo_uri= os.getenv("MONGODB_URI")
+    mongo_uri= st.secrets["MONGODB_URI"]
     client = MongoClient(mongo_uri)
     return client
